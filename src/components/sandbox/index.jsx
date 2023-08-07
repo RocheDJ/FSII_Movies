f543538a0125901d9a23aece9901f0d3000cd458
David Roche <rochedjd@gmail.com>
2023-08-07 12:19:45 +0100

    sandbox caseing
    Netlify deploy error correction

--- src/components/sandbox/index.jsx
+++ src/components/sandbox/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import YoutubeEmbed from "./YoutubeEmbed";
+import YoutubeEmbed from "./youtubeEmbed";
 
 const SandBox = ( {AppIsTV}) => {
     console.log("sandbox" ,{AppIsTV} );
